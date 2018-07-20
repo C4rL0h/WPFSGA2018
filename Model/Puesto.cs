22187e6fa0f401e1391e613e3f3daabb88a96095
Carlos Huit <carloshuit513@yahoo.com>
2018-07-20 00:47:05 -0600

    Iniciando el proyecto final y creando Modelo

---
+++ Model/Puesto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Model
+{
+    public class Puesto
+    {
+        #region "Propiedades"
+        public int PuestoId { get; set; }
+        public string Descripcion { get; set; }
+        #endregion
+    }
+}
