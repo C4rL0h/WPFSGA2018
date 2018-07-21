0fe64c5d023b487ac8450b962bd0ba08e7ac63e8
Carlos Huit <carloshuit513@yahoo.com>
2018-07-20 23:43:06 -0600

    Agregando entidades y Ventana Alumnos

---
+++ Model/ClaseAlumno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace ProyectoFinal.Model
+{
+    public class ClaseAlumno
+    {
+        public int AlumnoId { get; set; }
+        public int ClaseId { get; set; }
+        public DateTime FechaAsignacion { get; set; }
+        public virtual Alumno Alumno { get; set; }
+        public virtual Clase Clase { get; set; }
+    }
+}
--- Model/DbAlumnosContext.cs
+++ Model/DbAlumnosContext.cs
@@ -3,19 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
-
 namespace ProyectoFinal.Model
 {
-    public class DbAlumnosContext : DbContext
+    public class SGADataContext : DbContext
     {
-        public DbSet<Puesto> Puestos { get; set; }
+        public DbSet<Alumno> Alumnos { get; set; }
+        public DbSet<Carrera> Carreras { get; set; }
+        public DbSet<Salon> Salones { get; set; }
+        public DbSet<ClaseAlumno> ClasesAlumnos { get; set; }
+        public DbSet<GrupoAcademico> GrupoAcademicos { get; set; }
+        public DbSet<Clase> Clases { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-            modelBuilder.Entity<Puesto>().ToTable("Puestos").HasKey(p => new { p.PuestoId }).Property(d => d.Descripcion).IsRequired().HasMaxLength(256);
+            modelBuilder.Entity<Carrera>()
+                .ToTable("Carreras");
+            modelBuilder.Entity<Salon>()
+                .ToTable("Salones");
+            modelBuilder.Entity<Alumno>()
+                .ToTable("Alumnos")
+                .Property(c => c.Carne)
+                .IsRequired()
+                .HasMaxLength(7);
+            modelBuilder.Entity<Alumno>()
+                .ToTable("Alumnos")
+                .Property(n => n.Nombres)
+                .IsRequired()
+                .HasMaxLength(128);
+            modelBuilder.Entity<Alumno>()
+                .ToTable("Alumnos")
+                .Property(n => n.Apellidos)
+                .IsRequired()
+                .HasMaxLength(128);
+            modelBuilder.Entity<GrupoAcademico>()
+                .ToTable("GruposAcademicos");
+            modelBuilder.Entity<Clase>()
+                .ToTable("Clases");                
+            modelBuilder.Entity<ClaseAlumno>()
+                .ToTable("ClasesAlumnos")
+                .HasKey(x => new {x.AlumnoId,x.ClaseId });
         }
     }
 }
